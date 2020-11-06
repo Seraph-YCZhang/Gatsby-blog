d47a91265e2c4dee6d3f5a6b97721a58328a0652
Seraph-YCZhang <zyc199685@outlook.com>
2020-11-06 17:54:55 -0500

    add bg

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,31 +6,40 @@ const projects = [
   {
     title: "E-Commerce Website",
     description: "React/ Redux/ Firebase",
+    bg: "https://i.ibb.co/qrZMKGj/crwn.png",
   },
   {
     title: "Wechat Crawler",
-    description: "Python",
+    description: "Python/ MySQL",
+    bg:"https://smartproxy.com/wp-content/uploads/2020/06/Scraping-FAQ.svg"
   },
   {
     title: "Advertisement System",
-    description: "Java/ SpringCloud",
+    description: "Java/ SpringCloud/ MySQL",
+    bg:"https://thehimalayantimes.com/wp-content/uploads/2015/07/httpaimsportal.com_.jpg"
   },
   {
     title: "Invenst Club Website",
-    description: "Javascript/ PHP",
+    description: "Javascript/ PHP/ MySQL",
+    bg:"https://i.ibb.co/5LwNRzb/invub.png"
   },
 ]
 const Projects = () => {
   return (
     <Layout>
-    <Head title="Projects" />
+      <Head title="Projects" />
       <div className={styles.container}>
         <div className={styles.innerContainer}>
           {projects.map((proj, idx) => (
             <div className={styles.projectContainer} key={idx}>
-              <div></div>
-              <div className={styles.animateDown}>{proj.title}</div>
-              <div className={styles.animateUp}>{proj.description}</div>
+              <div className={styles.show}>
+                <div>{proj.title}</div>
+                <div>{proj.description}</div>
+              </div>
+              <div
+                className={styles.bg}
+                style={{ backgroundImage: `url('${proj.bg}')` }}
+              />
             </div>
           ))}
         </div>
