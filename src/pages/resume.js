29bcc1cbac80c5872c66c4f8de6f658903f87ec7
Seraph-YCZhang <zyc199685@outlook.com>
2020-11-06 15:48:31 -0500

    css

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,16 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
-import resume from "../../public/static/resume.jpg"
+import resume from "./resume.jpg"
 import Head from '../components/head'
 const Resume = () => {
   return (
     <Layout>
     <Head title="Resume" />
-      <div style={{ width: "100%", minHeight: "100vh" }}>
+      <div style={{ width: "100%", minHeight: "100vh", background: "#252934", padding:'3rem 0' }}>
         <div
           style={{
             backgroundImage: `url(${resume})`,
-            margin: "2rem auto",
             height: "100%",
             minHeight: '100vh',
             width: "auto",
