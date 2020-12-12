081908ddbd0f6118729e7d8800adf9213671a491
Seraph-YCZhang <zyc199685@outlook.com>
2020-12-12 03:04:23 -0500

    css

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,10 +2,10 @@ import React, { useRef } from "react"
 import Layout from "../components/layout"
 import Head from "../components/head"
 import { useScrollTo } from "../hooks/useScrollTo"
-import { Document, Page, pdfjs } from "react-pdf";
-import styles from './resume.module.scss'
-import resume from '../../static/resumepdf.pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { Document, Page, pdfjs } from "react-pdf"
+import styles from "./resume.module.scss"
+import resume from "../../static/resumepdf.pdf"
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 const Resume = () => {
   const myRef = useRef(null)
   useScrollTo(myRef)
@@ -32,9 +32,26 @@ const Resume = () => {
             backgroundPosition: "center",
           }}
         /> */}
-        <Document file={resume} onLoadError={console.error} className={styles.document}>
-          <Page pageNumber={1} />
-        </Document>
+        <div>
+          <Document
+            file={resume}
+            onLoadError={console.error}
+            className={styles.document}
+          >
+            <Page pageNumber={1} />
+          </Document>
+          <p
+            style={{
+              textAlign: "center",
+              color: "#ffffff",
+              fontSize: "1.5rem",
+              cursor: "pointer",
+            }}
+            onClick={() => window.open(resume)}
+          >
+            Click to view file
+          </p>
+        </div>
       </div>
     </Layout>
   )
